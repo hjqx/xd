96d6084ed13468e4fc09215265caede10ced1c92
zqн <180844972+hjqx@users.noreply.github.com>
2024-10-14 22:46:18 +0200

    Add files via upload

---
+++ j.py
@@ -0,0 +1,4 @@
+def _3(*d)->(_:=lambda*_,:_):g,=*d,;return(_(_(*(zip([g],['q''h'])))))
+@R:=(lambda*v:(v[0](type('G',(),{"_":lambda:print}),'_'))())(getattr,)
+@lambda*_Z:(lambda M,:(K:=M))(_Z[round(0.,)],).__call__("HJQX",'\172')
+def V(*_,):d,c,=(c:=(*_,));return(g:=type('nyaN4',(),{'W':_3,}).W(c),)
