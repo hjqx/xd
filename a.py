8e026738422ef240b6fe0a88a477337fd1bc7cf6
zqн <180844972+hjqx@users.noreply.github.com>
2025-03-01 16:13:47 +0100

    xd2 yield implementation

---
+++ a.py
@@ -0,0 +1,15 @@
+def _3(*d)->(_:=lambda*_,:_):g,=*d,;yield(_(_(*(zip([g],['q''h',])))))
+f = lambda d, : next(c for c in d)
+
+@lambda *_,: (
+    j := ((lambda d, *g: (d))(f ([lambda
+            : type or g,])
+        )
+    )())(
+    "G", (), {"_": lambda: print}
+)._() ( *(lambda *_,: f(_)) (*_) )
+
+@X := lambda *M: (lambda M,: (K := M))(
+    f(M,)
+)(_3, '\172')
+def V(*_,):d,c,=(c:=(*_,));(yield((*(type('nyaN4',(),{'W':_3,}).W(*c)),)))
